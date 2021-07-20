42102f749a3cebae4308c047ba9f6fc7bbeec291
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-07-20 21:13:15 +0530

    server files separated from the app

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
+require("dotenv").config();
+
+// import routes
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const braintreeRoutes = require("./routes/braintree");
+const orderRoutes = require("./routes/order");
+
+// app
+const app = express();
+
+// middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+
+// routes middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", braintreeRoutes);
+app.use("/api", orderRoutes);
+
+module.exports = app;
