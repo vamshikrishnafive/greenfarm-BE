2566c128b07ddd57bfaf43670e44ab81fdddc31e
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-07-20 20:47:30 +0530

    User controllers updated

--- routes/user.js
+++ routes/user.js
@@ -19,7 +19,6 @@ router.get("/secret/:userId", requireSignin, isAuth, isAdmin, (req, res) => {
 router.get("/user/:userId", requireSignin, isAuth, read);
 router.put("/user/:userId", requireSignin, isAuth, update);
 router.get("/orders/by/user/:userId", requireSignin, isAuth, purchaseHistory);
-
 router.param("userId", userById);
 
 module.exports = router;
