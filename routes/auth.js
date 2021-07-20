33b6610ce18c93143d12bf3dcb471958749b2331
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-07-20 17:35:46 +0530

    authentication updated

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-    signup,
-    signin,
-    signout,
-    requireSignin
-} = require("../controllers/auth");
+const Authenticate = require("../controllers/auth");
 const { userSignupValidator } = require("../validator");
 
-router.post("/signup", userSignupValidator, signup);
-router.post("/signin", signin);
-router.get("/signout", signout);
+router.route("/signup").post(userSignupValidator, Authenticate.signup);
+router.route("/signin").post(Authenticate.signin);
+router.route("/signout").get(Authenticate.signout);
 
 module.exports = router;
