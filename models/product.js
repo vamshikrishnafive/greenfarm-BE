9ab5ed7b7406d12468113f0b9505c7d999b65387
vamshi krishna <53006565+vamshikrishnafive@users.noreply.github.com>
2021-07-24 22:53:09 +0530

    increased description length and name length

--- models/product.js
+++ models/product.js
@@ -7,12 +7,12 @@ const productSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      maxlength: 32,
+      maxlength: 50,
     },
     description: {
       type: String,
       required: true,
-      maxlength: 2000,
+      maxlength: 5000,
     },
     price: {
       type: Number,
